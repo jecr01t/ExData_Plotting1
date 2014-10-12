712559eebd537bd7c03aadbe7b9b1217c7de9d2b
jecr01t <hjecr01t@gmail.com>
2014-10-12 15:36:24 -0400

    fixes

--- myplots/plot3.R
+++ myplots/plot3.R
@@ -11,8 +11,8 @@ plot(
 	ylab="Energy sub metering",
 	ylim=c(0,30), axes=F, xlab="", frame.plot=TRUE
 )
-lines(powerConsumption$DateTime,powerConsumption$Sub_metering_2, col="blue")
-lines(powerConsumption$DateTime,powerConsumption$Sub_metering_3, col="green")
+lines(powerConsumption$DateTime,powerConsumption$Sub_metering_2, col="green")
+lines(powerConsumption$DateTime,powerConsumption$Sub_metering_3, col="blue")
 
 axis.POSIXct(1,
 		at=c("2007-02-01 00:00:00", "2007-02-02 00:00:00", "2007-02-03 00:00:00"),
@@ -20,7 +20,7 @@ axis.POSIXct(1,
 
 axis(side=2, at=c(0,10,20,30))
 
-legend(	"topright", 	lwd=1,	col = c("black", "blue", "green"),
+legend(	"topright", 	lwd=1,	col = c("black", "green", "blue"),
 		legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3")
 )
 
